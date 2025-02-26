e59b3d5ebf5c6a0fd413951a4c7fa5de1b85ab81
geniuls-oficial <geniuls.official@gmail.com>
2025-02-27 01:05:39 +0530

    baseUrl fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
-  // baseURL: 'https://server-nodevbuild-2.onrender.com',
+  // baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://server-nodevbuild-2.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
