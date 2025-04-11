49c20d41969d53becc7967530a1cb87c14d3ad23
BuildWithFaiz <buildwithfaiz@gmail.com>
2025-04-11 10:49:04 +0530

    firebase credentials fix

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBCjNXEXUWVNjYqEoRxLXYZxEJPwGVyJxY",
+  apiKey: "AIzaSyCYlOLvglHyq8ccCH-NJBE7Uy-nRi82ABE",
   authDomain: "nodevbuildweb.firebaseapp.com",
   projectId: "nodevbuildweb",
-  storageBucket: "nodevbuildweb.appspot.com",
-  messagingSenderId: "1098979667674",
-  appId: "1:1098979667674:web:a6d65c6d0b4dd06c9f6c2e",
-  measurementId: "G-RLXW6R5QE2"
+  storageBucket: "nodevbuildweb.firebasestorage.app",
+  messagingSenderId: "1027291490444",
+  appId: "1:1027291490444:web:a8179a380a28ebadb2f3a9",
+  measurementId: "G-G3T68V9SL6"
 };
 
 // Initialize Firebase
